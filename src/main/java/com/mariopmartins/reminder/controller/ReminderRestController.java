802dab7e5307fffe0c304b3db02469f05f02ad9a
Mario Martins <mariopmartins@gmail.com>
2018-07-11 22:15:03 -0400

    Added more documentation to the swagger ui

--- src/main/java/com/mariopmartins/reminder/controller/ReminderRestController.java
+++ src/main/java/com/mariopmartins/reminder/controller/ReminderRestController.java
@@ -21,6 +21,8 @@ import com.mariopmartins.reminder.domain.Reminder;
 import com.mariopmartins.reminder.domain.ReminderStatus;
 import com.mariopmartins.reminder.service.ReminderService;
 
+import io.swagger.annotations.ApiOperation;
+
 @RestController
 @RequestMapping("/api/reminders")
 public class ReminderRestController {
@@ -29,25 +31,29 @@ public class ReminderRestController {
 	private ReminderService reminderService;
 	
 	@GetMapping("/{id}")
+	@ApiOperation(value = "Find a Reminder by id", response = Reminder.class)
 	public ResponseEntity<Reminder> get(
 			@PathVariable(required=true) Long id) {
 		return ResponseEntity.ok(reminderService.findById(id));
 	}
 	
 	@GetMapping
+	@ApiOperation(value = "List Reminders by dueDate and/or status", notes = "Only present parameters will be used to filter the reminders.", response = Reminder.class, responseContainer = "Iterable")
 	public ResponseEntity<Iterable<Reminder>> list(
-			@RequestParam(required = false) @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate dueDate,
-			@RequestParam(required = false) ReminderStatus status) {
+			@RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate dueDate,
+			@RequestParam ReminderStatus status) {
 		return ResponseEntity.ok(reminderService.findAll(dueDate, status));
 	}
 
 	@PostMapping
+	@ApiOperation(value = "Add a Reminder", response = Reminder.class)
 	public ResponseEntity<Reminder> add(
 			@RequestBody @Valid Reminder reminder) {
 		return ResponseEntity.ok(reminderService.save(reminder));
 	}
 
 	@PutMapping("/{id}")
+	@ApiOperation(value = "Update a Reminder", response = Reminder.class)
 	public ResponseEntity<Reminder> update(
 			@PathVariable(required=true) Long id, 
 			@RequestBody @Valid Reminder reminder) {
@@ -55,6 +61,7 @@ public class ReminderRestController {
 	}
 	
 	@DeleteMapping("/{id}")
+	@ApiOperation(value = "Delete a Reminder", response = Reminder.class)
 	public void delete(
 			@PathVariable(required=true) Long id) {
 		reminderService.deleteById(id);
