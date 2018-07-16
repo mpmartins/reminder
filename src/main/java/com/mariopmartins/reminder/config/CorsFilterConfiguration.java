245acb6bd6c17e31616448c1cf986003e071f836
Mario Martins <mariopmartins@gmail.com>
2018-07-16 00:28:52 -0400

    Using a simple filter to setup cors

---
+++ src/main/java/com/mariopmartins/reminder/config/CorsFilterConfiguration.java
@@ -0,0 +1,24 @@
+package com.mariopmartins.reminder.config;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsFilterConfiguration {
+
+	@Bean
+	public FilterRegistrationBean<CorsFilter> corsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		config.addAllowedOrigin("*");
+		config.addAllowedMethod("*");
+		source.registerCorsConfiguration("/api/**", config);
+		FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<CorsFilter>(new CorsFilter(source));
+		bean.setOrder(0);
+		return bean;
+	}
+}
